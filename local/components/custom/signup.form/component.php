5cd276a8e07661a0fa3e93e8413467e7fb5f03ba
ViktorPankratov <pankratov.viktor.97@gmail.com>
2017-08-21 08:55:51 +0400

    Add register

--- local/components/custom/signup.form/component.php
+++ local/components/custom/signup.form/component.php
@@ -17,3 +17,21 @@
     </div> 
 </form>
 <a href="/">Вернуться на главную</a>
+
+
+<?
+if(isset($_POST['submit'])){
+    $user = new CUser;
+    $arFields = Array(
+    "NAME"              => $_POST['name'],
+    "EMAIL"             => $_POST['email'],
+    "LOGIN"             => $_POST['login'],
+    "PASSWORD"          => $_POST['password']
+    );
+    $ID = $user->Add($arFields);
+    if (intval($ID) > 0)
+        echo "</br>" . "Пользователь успешно добавлен.";
+    else
+        echo $user->LAST_ERROR;
+}
+?>
