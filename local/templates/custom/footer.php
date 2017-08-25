0c0121bb6964a5d1eedc6bd5e069e70c15118404
ViktorPankratov <pankratov.viktor.97@gmail.com>
2017-08-25 11:58:49 +0400

    update footer style

--- local/templates/custom/footer.php
+++ local/templates/custom/footer.php
@@ -1,3 +1,3 @@
 <header class="footer">
-		<strong>Footer:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tortor. Praesent dictum, libero ut tempus dictum, neque eros elementum mauris, quis mollis arcu velit ac diam. Etiam neque. Quisque nec turpis. Aliquam arcu nulla, dictum et, lacinia a, mollis in, ante. Sed eu felis in elit tempor venenatis. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut ultricies porttitor purus. Proin non tellus at ligula fringilla tristique. Fusce vehicula quam. Curabitur vel tortor vitae pede imperdiet ultrices. Sed tortor.
+		<b>&copy; 2002 Битрикс, 2007 <a href="http://www.1c-bitrix.ru">1С-Битрикс</a></b>
 </header>
