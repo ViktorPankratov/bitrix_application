1b67f2aee0e57bbc6a92f06d01024dfd59ccb96e
ViktorPankratov <pankratov.viktor.97@gmail.com>
2017-08-25 11:33:21 +0400

    update nav menu

--- local/templates/custom/header.php
+++ local/templates/custom/header.php
@@ -1,13 +1,19 @@
+
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+
 <!DOCTYPE html>
-<html>
 <head>
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<?$APPLICATION->ShowHead()?>
-<title><?$APPLICATION->ShowTitle()?></title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<?$APPLICATION->ShowHead()?>
+	<title><?$APPLICATION->ShowTitle()?></title>
 </head>
 <div id="panel"><?$APPLICATION->ShowPanel();?></div>
-<h1><?$APPLICATION->ShowTitle(false)?></h1>
 <header class="header">
-		<strong>Header:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tortor. Praesent dictum, libero ut tempus dictum, neque eros elementum mauris, quis mollis arcu velit ac diam. Etiam neque. Quisque nec turpis. Aliquam arcu nulla, dictum et, lacinia a, mollis in, ante. Sed eu felis in elit tempor venenatis. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut ultricies porttitor purus. Proin non tellus at ligula fringilla tristique. Fusce vehicula quam. Curabitur vel tortor vitae pede imperdiet ultrices. Sed tortor.
+		<nav class="top-menu">
+		  <ul class="menu-main">
+		    <li><a href="/">Главная</a></li>
+		    <li><a href="/register.php">Регистрация</a></li>
+		    <li><a href="/signin.php">Авторизация</a></li>
+		  </ul>
+		</nav>
 </header>
