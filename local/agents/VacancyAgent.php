05c974c7049ea3a159f81955226188a3e556704a
ViktorPankratov <pankratov.viktor.97@gmail.com>
2017-09-11 19:10:35 +0400

    Add vacancy update funtion to vacancyAgent

--- local/agents/VacancyAgent.php
+++ local/agents/VacancyAgent.php
@@ -1,6 +1,21 @@
 <?
-namespace Local\Agents;
-class vacancyAgent{
+class VacancyAgent {
 	public static function updateActive(){
+		CModule::includeModule('iblock');
+		$ID = 'vacancy'; 
+		$IBLOCK_TYPE = 'vacancy';
+		$arFilter = array("IBLOCK_TYPE" => $IBLOCK_TYPE, "ID" => $IBLOCK_ID);
+		$res = CIBlockElement::GetList(array(), $arFilter, false, array(), array());
+		while ($vacancyElement = $res->GetNextElement()){
+			$vacancyElement = $vacancyElement->GetFields();
+			if (isset($vacancyElement[ACTIVE_TO])){
+				$currentDT = date("d.m.Y H:i:s");
+				if($currentDT <= $vacancyElement[ACTIVE_TO]){
+					$activity = array( "ACTIVE" => "N", );
+					$updateVac = new CIBlockElement();
+					$updateVac->Update($vacancyElement[ID], $activity);
+				}
+			}
+		} 
 	}
 }
