a43fca258557ac92157e051aa3e0d2031b19acbe
ViktorPankratov <pankratov.viktor.97@gmail.com>
2017-09-19 11:03:10 +0400

    responses now uniqe

--- local/ajax/get_respond_btn.php
+++ local/ajax/get_respond_btn.php
@@ -11,11 +11,10 @@
 		$idv = $_GET['idv'];
 		$res = $DB->query('SELECT COUNT(*) FROM v_response WHERE user_id = ' . $USER->GetID() . ' AND vacancy_id = ' . $idv .'');
 		$res = $res->getNext();
-		if( $res['COUNT(*)'] < 0 ){
+		if( $res['COUNT(*)'] > 0 ){
 			$ans = '<a>Вы откликнулись</a>';
 		}else{
 			$ans = '<a href="#response" id="open_modal">Откликнуться</a>';
-			// $ans = '<input type="submit" onclick="Dialog.Show();" id="responseBtn" value="Откликнуться">';
 		}
 		echo json_encode($ans);
 	}
