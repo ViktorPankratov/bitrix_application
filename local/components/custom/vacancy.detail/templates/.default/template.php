0f86d9514b44290195c1d9f55e3b9726cfc9fc41
ViktorPankratov <pankratov.viktor.97@gmail.com>
2017-09-19 10:52:33 +0400

    Add validator to response form

--- local/components/custom/vacancy.detail/templates/.default/template.php
+++ local/components/custom/vacancy.detail/templates/.default/template.php
@@ -3,17 +3,19 @@
 <div id="response"> 
 	<span id="modal_label">Откликнуться на вакансию</span>
 	<span id="modal_close">X</span>
-    <form method="POST">
+    <form method="POST" id="resp_form">
     	<div class="excited_price">
 		<a>Желаемая зарплата :</a>
-			от <input type="text" id="price_from" class="price_field" maxlength="15" onkeypress="if((event.keyCode < 48)||(event.keyCode > 57)) event.returnValue=false"/>
-			до <input type="text" id="price_to" class="price_field" maxlength="15" onkeypress="if((event.keyCode < 48)||(event.keyCode > 57)) event.returnValue=false"/> &#8381
+			от <input type="text" data-validation="number" id="price_from" class="price_field" maxlength="15"/>
+			до <input type="text" data-validation="number" id="price_to" class="price_field" maxlength="15"/> &#8381
 		</div>
 		<input type="hidden" id="idVacancy" name="v_id" value="<? echo $arResult["VACANCY"]["ID"]; ?>" />
 		<input type="hidden" id= "idUser" name="u_id" value="<? echo $USER->getID() ?>" />
-		<textarea id="resp_text" name="r_text" maxlength="2000" placeholder="Введите текст сообщения(не более 2000 символов)"></textarea>		
-		<input id="resp_button" type="button" value="Откликнуться" />
-	</form>	
+		<textarea data-validation="length" data-validation-length="50-2000" id="resp_text" name="r_text" placeholder="Введите текст сообщения(от 50 до 2000 символов)"></textarea>		
+		<input id="resp_button" type="submit" value="Откликнуться" />
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
+	</form>
 </div>
 <div id="overlay"></div>
 <? echo $arResult["VACANCY"]["NAME"];?><br>
