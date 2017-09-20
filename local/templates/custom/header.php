3f926f0bd266c586d470260fd266f5f353b2682b
ViktorPankratov <pankratov.viktor.97@gmail.com>
2017-09-20 22:14:02 +0400

    Create employers direction

--- local/templates/custom/header.php
+++ local/templates/custom/header.php
@@ -15,7 +15,39 @@
 		    <li><a href="/">Главная</a></li>
 		    <li><a href="/register.php">Регистрация</a></li>
 		    <li><a href="/signin.php">Авторизация</a></li>
-		    <li><a href="/vacancies/">Вакансии</a></li>
+		    <li><a href="/vacancies">Вакансии</a></li>
+		    <?
+			    global $USER;
+				$current_user = $USER->getID();
+				$arGroupsId = CUser::GetUserGroup($current_user);
+				foreach ($arGroupsId as $key => $value) {
+					$arGroups = CGroup::GetByID($value);
+					$arGroups = $arGroups->fetch();
+					$arGroup_names[] = $arGroups['STRING_ID'];
+				}
+				if (in_array('employers', $arGroup_names)){
+					echo '<li class="vert-nav"><a href="#">Работодателю</a>
+					    	<ul>
+					    		<li><a href="/vacancies/vacancy">Вакансии</a></li>
+						    	<li><a href="/vacancies/response">Отклики  </a></li>
+						    </ul>
+					    </li>';
+				}
+			?>		    
 		  </ul>
 		</nav>
 </header>
+<script type="text/javascript">
+$(document).ready(function () {
+ 
+  $('.vert-nav').hover(
+    function() {
+      $('ul', this).slideDown(110);
+    },
+    function() {
+      $('ul', this).slideUp(110);
+    }
+  );
+ 
+});
+</script>
--- urlrewrite.php
+++ urlrewrite.php
@@ -1,6 +1,12 @@
 <?
 $arUrlRewrite = array(
 	array(
+		"CONDITION" => "#^/vacancies/vacancy/([0-9]+)#",
+		"RULE" => "ELEMENT_ID=\$1",
+		"ID" => "",
+		"PATH" => "/vacancies/vacancy/index.php",
+	),
+	array(
 		"CONDITION" => "#^/vacancies/([0-9]+)#",
 		"RULE" => "ELEMENT_ID=\$1",
 		"ID" => "",
