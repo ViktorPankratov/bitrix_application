e8fd87be71a35c28b3348f2a39b1b95249241915
ViktorPankratov <pankratov.viktor.97@gmail.com>
2017-10-01 18:48:26 +0400

    Add some styles to site

--- local/layouts/signup/signup.php
+++ local/layouts/signup/signup.php
@@ -1,17 +1,33 @@
-<form  action="<?php echo $_SERVER['PHP_SELF'];?>" method="post"> 
-    <div> 
-    <input required type="text" name="name" placeholder="Имя"> 
-    </div> 
-    <div> 
-    <input required type="text" name="login" placeholder="Логин"> 
-    </div>  
-    <div> 
-    <input required type="email" name="email" placeholder="email адрес"> 
-    </div> 
-    <div> 
-    <input required type="password" name="password" placeholder="пароль"> 
-    </div> 
-    <button type="submit" name='submit' class="form">Зарегистрироваться</button>
-    </div> 
-</form>
-<a href="/">На главную</a>
+<style type="text/css">
+    .reg_form{
+        height: 200px;
+        margin-bottom: 10px;
+    }
+    h1{
+        font-size: 20px;
+    }
+    .reg_form div input, button{
+        margin-top: 10px;
+        margin-left: 230px;
+        width: 200px;
+    }
+</style>
+<div class="wrapper">
+    <form  action="<?php echo $_SERVER['PHP_SELF'];?>" method="post" class="reg_form"> 
+        <h1>Регистрация</h1>
+        <div> 
+        <input required type="text" name="name" placeholder="Имя"> 
+        </div> 
+        <div> 
+        <input required type="text" name="login" placeholder="Логин"> 
+        </div>  
+        <div> 
+        <input required type="email" name="email" placeholder="email адрес"> 
+        </div> 
+        <div> 
+        <input required type="password" name="password" placeholder="пароль"> 
+        </div> 
+        <button type="submit" name='submit' class="form">Зарегистрироваться</button>
+        </div> 
+    </form>
+</div>
